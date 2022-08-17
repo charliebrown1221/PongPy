7389fa9267be1e027a8941ad42058a3d63566320
charliebrown1221 <chaimbrown1@yahoo.com>
2022-08-17 10:43:26 -0400

    <feat> added sound

--- pong.py
+++ pong.py
@@ -1,4 +1,6 @@
 import turtle
+import winsound
+
 
 wn = turtle.Screen()
 wn.title("Pong")
@@ -85,10 +87,12 @@ while True:
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
+        winsound.PlaySound("bounce.wav" ,winsound.SND_ASYNC)
 
     if ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1   
+        winsound.PlaySound("bounce.wav" ,winsound.SND_ASYNC)
 
 
     if ball.xcor() >390:
@@ -110,7 +114,9 @@ while True:
     if (ball.xcor() > 340 and ball.xcor()< 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() > paddle_b.ycor() -40):
         ball.setx(340)
         ball.dx *= -1
+        winsound.PlaySound("bounce.wav" ,winsound.SND_ASYNC)
 
     if (ball.xcor() < -340 and ball.xcor()> -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() > paddle_a.ycor() -40):
         ball.setx(-340)
         ball.dx *= -1
+        winsound.PlaySound("bounce.wav" ,winsound.SND_ASYNC)
