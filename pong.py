078a6cb30bec4c7c9b2f999a5fd03055567117a5
charliebrown1221 <chaimbrown1@yahoo.com>
2022-08-17 09:46:48 -0400

    <feat> ball moves

--- pong.py
+++ pong.py
@@ -31,6 +31,9 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0,0)
+ball.dx = 0.1
+ball.dy = 0.1
+
 
 def paddle_a_up():
     y = paddle_a.ycor()
@@ -63,3 +66,6 @@ wn.onkeypress(paddle_b_down,"Down")
 
 while True:
     wn.update()
+
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
